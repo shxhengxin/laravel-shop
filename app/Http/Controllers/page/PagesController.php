34e3fab2ec5a7517246b5fba0a04b8e9998f4a9c
shenhengxin <shenhengxin@qq.com>
2018-09-13 15:02:03 +0800

    邮箱验证中间件

--- app/Http/Controllers/page/PagesController.php
+++ app/Http/Controllers/page/PagesController.php
@@ -10,4 +10,9 @@ class PagesController extends Controller
     public function root() {
         return view('pages.root');
     }
+
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@
 });*/
 Route::get('/', 'page\PagesController@root')->name('root');
 Auth::routes();
+Route::group(['middleware' => 'auth','namespace'=>'page'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    //开始 测试用的 确保登录了
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+    //结束 测试
+
+});
