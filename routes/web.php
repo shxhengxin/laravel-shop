32d99f7ead4c3c59709285248fe9aa9b39448e5f
shenhengxin <shenhengxin@qq.com>
2018-10-03 17:12:13 +0800

    用户界面 - 订单详情页

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::group(['namespace'=>'order'],function (){
             Route::post('orders', 'OrdersController@store')->name('orders.store');
             Route::get('orders', 'OrdersController@index')->name('orders.index');
+            Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
         });
 
     });
