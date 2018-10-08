0b2dadba84d8b154fed704f0ee9553d684cf3d07
shenhengxin <shenhengxin@qq.com>
2018-10-08 17:32:26 +0800

    管理后台 - 同意退款 - 微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -71,5 +71,6 @@ Route::group(['middleware' => 'auth'], function() {
 Route::get('products', 'product\ProductsController@index')->name('products.index');//商品列表
 Route::get('products/{product}', 'product\ProductsController@show')->name('products.show');//商品详情
 
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 Route::post('payment/alipay/notify', 'pay\PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
