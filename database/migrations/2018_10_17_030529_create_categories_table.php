a038064f17032481ed84c5a5126ce8f5abdb9211
shenhengxin <shenhengxin@qq.com>
2018-10-17 11:18:47 +0800

    初始化类目模型

--- database/migrations/2018_10_17_030529_create_categories_table.php
+++ database/migrations/2018_10_17_030529_create_categories_table.php
@@ -14,7 +14,13 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->comment('	自增长ID');
+            $table->string('name')->comment('类目名称');
+            $table->unsignedInteger('parent_id')->nullable()->comment('	父类目ID');
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->boolean('is_directory')->comment('是否拥有子类目');
+            $table->unsignedInteger('level')->comment('	当前类目层级');
+            $table->string('path')->comment('该类目所有父类目 id');
             $table->timestamps();
         });
     }
@@ -29,3 +35,35 @@ class CreateCategoriesTable extends Migration
         Schema::dropIfExists('categories');
     }
 }
+/**
+ * [
+[
+"id" => 1,
+"name" => "手机配件",
+"parent_id" => null,
+"level" => 0,
+"path" => "-"
+],
+[
+"id" => 2,
+"name" => "耳机",
+"parent_id" => 1,
+"level" => 1,
+"path" => "-1-"
+],
+[
+"id" => 3,
+"name" => "蓝牙耳机",
+"parent_id" => 2,
+"level" => 2,
+"path" => "-1-2-"
+],
+[
+"id" => 4,
+"name" => "移动电源",
+"parent_id" => 1,
+"level" => 1,
+"path" => "-1-"
+],
+];
+ */
