5c2bcf42e89e002089eaa9be93496278d4e2fc8f
shenhengxin <shenhengxin@qq.com>
2018-10-17 17:21:09 +0800

    商品后台添加类目字段

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,6 +16,8 @@ $factory->define(App\Models\Product::class, function (Faker $faker) {
         "https://lccdn.phphub.org/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
     ];
     $image = $faker->randomElement($images);
+    // 从数据库中随机取一个类目
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
 
     return [
         'title'        => $faker->word,
@@ -26,5 +28,6 @@ $factory->define(App\Models\Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        'category_id'  => $category ? $category->id : null,
     ];
 });
